5c042458577b65c5532a082f6619c92fe0b77ded
fatima204 <fatima.amrin.hossain@gmail.com>
2023-05-01 22:32:49 +0200

    Klasse WatchListEntity

---
+++ src/main/java/at/ac/fhcampuswien/fhmdb/WatchListEntity.java
@@ -0,0 +1,15 @@
+package at.ac.fhcampuswien.fhmdb;
+
+public class WatchListEntity {
+
+    private long id;
+    private String aplid;
+    private String title;
+    private String descritption;
+    private String genres;
+    private int releaseYear;
+    private String imgURL;
+    private int lengthInMinutes;
+    private double rating;
+
+}
