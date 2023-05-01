f373e5ad690d7baa69fe45445bff035d4f705923
fatima204 <fatima.amrin.hossain@gmail.com>
2023-05-01 22:28:43 +0200

    Klasse Database

---
+++ src/main/java/at/ac/fhcampuswien/fhmdb/Database.java
@@ -0,0 +1,11 @@
+package at.ac.fhcampuswien.fhmdb;
+
+public class Database {
+
+    private String DB_URL;
+    private String username;
+    private String password;
+    //private ConnectionSource connectionSource;
+    //private Dao <WatchlistEntity,Long> dao;
+
+}
